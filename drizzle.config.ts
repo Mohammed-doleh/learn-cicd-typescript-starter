fa01f6be7650888372b0ef4ea14f91f1f82e563d
Mohammed Doleh <dole9123@gamil.com>
2026-04-05 19:03:38 +0300

    fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 import dotenv from "dotenv";
 dotenv.config();
 
-export default defineConfig({
+export default {
   out: "./src/db/migrations",
   schema: "./src/db/schema.ts",
   dialect: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL || "",
   },
-});
+} satisfies Config;
