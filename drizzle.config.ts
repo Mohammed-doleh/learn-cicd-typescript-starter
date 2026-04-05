65dd5de4b77a4ea771d22d00a8526557dd786dda
Mohammed Doleh <dole9123@gamil.com>
2026-04-05 19:24:53 +0300

    fix drizzle config to separate authToken

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,16 @@ import type { Config } from "drizzle-kit";
 import dotenv from "dotenv";
 dotenv.config();
 
+const url = new URL(process.env.DATABASE_URL || "");
+const authToken = url.searchParams.get("authToken") || "";
+url.searchParams.delete("authToken");
+
 export default {
   out: "./src/db/migrations",
   schema: "./src/db/schema.ts",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "",
+    url: url.toString(),
+    authToken: authToken,
   },
 } satisfies Config;
